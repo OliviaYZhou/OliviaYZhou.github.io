633b792ce8145f3a0e87ad5b0e2827f5e9e036a1
Olivia <oliviayzhou@gmail.com>
2024-08-25 00:03:18 -0400

    home, projects page, experience page done, updated experience, adding projects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+import "./styles/CommonStyles.css"
+import * as Mui from "@mui/material"
+import HomePage from "./react-components/HomePage"
+import Header from "./react-components/Header"
+import Projects from "./react-components/Projects"
+import Alert from "@mui/material/Alert"
+import AlertTitle from "@mui/material/AlertTitle"
+import Stack from "@mui/material/Stack"
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import reportWebVitals from "./reportWebVitals"
+import Skills from "./react-components/Skills"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            {/* <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <Mui.Button variant="contained">Hello World</Mui.Button>
+                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                        Learn React
+                    </a>
+                </header>
+            </div> */}
+            <Header />
+            <Alert severity="info">
+                This website is still under development. Existing pages are functional but
+                formatting and content are still being updated. Skill Button clicks will work soon!
+            </Alert>
+            <HomePage />
+            {/* <Projects /> */}
+        </div>
+        // <BrowserRouter>
+        //     <Routes>
+        //         <Route exact path="/" element={<HomePage />} />
+        //         <Route exact path="/skills" element={<Skills />} />
+        //         <Route exact path="/projects" element={<Projects />} />
+        //         {/* <Route exact path="/bio" render={() => < />} /> */}
+        //         {/* <Route exact path="/book/view" render={() => <BookViewer />} /> */}
+        //     </Routes>
+        // </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import Skills from "./react-components/Skills"
+import Projects from "./react-components/Projects"
+import ExperiencePage from "./react-components/experience"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(
+//     // <React.StrictMode>
+//     <App />
+//     // {/* </React.StrictMode> */}
+// )
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <BrowserRouter>
+        <Routes>
+            <Route exact path="/" element={<App />} />
+            <Route exact path="/skills" element={<Skills />} />
+            <Route exact path="/projects" element={<Projects />} />
+            <Route exact path="/experience" element={<ExperiencePage />} />
+            {/* <Route exact path="/bio" render={() => < />} /> */}
+            {/* <Route exact path="/book/view" render={() => <BookViewer />} /> */}
+        </Routes>
+    </BrowserRouter>,
+    document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
